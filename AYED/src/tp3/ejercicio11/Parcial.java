2610ae36d79ef96521f370d571a67fb41730a042
David <alvarez.david@alu.ing.unlp.edu.ar>
2024-05-08 10:49:31 -0300

    Actualizacion 8/5/24

---
+++ AYED/src/tp3/ejercicio11/Parcial.java
@@ -0,0 +1,39 @@
+package tp3.ejercicio11;
+import java.util.*;
+import tp3.ejercicio2.GeneralTree;
+import tp1.ejercicio8.Queue;
+
+public class Parcial {
+	
+	public static boolean resolver(GeneralTree<Integer> arbol) {
+		boolean result = true; int contador = 0, nivel = 0; 
+		
+		Queue<GeneralTree<Integer>> queue = new Queue<GeneralTree<Integer>>(); 
+		GeneralTree<Integer> tree_aux; 
+		queue.enqueue(arbol);
+		queue.enqueue(null);
+		while (!queue.isEmpty()) {
+			tree_aux = queue.dequeue(); 
+			if (tree_aux != null) {
+				List<GeneralTree<Integer>> children = tree_aux.getChildren();
+				for (GeneralTree<Integer> child : children) {
+					contador++;
+					queue.enqueue(child);
+				}
+			} else {
+				nivel++; //Nivel_de_nodos_que_se_guardo_Si_se_eran_hojas_no_se_guarda_null
+				if (contador > 0) {
+					queue.enqueue(null);
+					if (nivel+1 != contador) {
+						result = false; 
+						break; 
+					}
+					contador = 0; 
+				}
+			}
+			
+		}
+		return result; 
+	}
+
+}
